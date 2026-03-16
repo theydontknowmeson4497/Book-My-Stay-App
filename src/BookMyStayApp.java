9d383132a16bfc85095a99f4aa041c4b8ca62ecb
theydontknowmeson4497 <mm4497@srmist.edu.in>
2026-03-16 15:52:40 +0530

    UC2

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,11 +1,73 @@
 public class BookMyStayApp {
 
     public static void main(String[] args) {
-        System.out.println("======================================");
-        System.out.println("     Welcome to Book My Stay App     ");
-        System.out.println("     Hotel Booking System v1.0       ");
-        System.out.println("======================================");
-        System.out.println("Application started successfully.");
-        System.out.println("Application exiting... Thank you!");
+
+        System.out.println("==================================");
+        System.out.println("Book My Stay - Hotel Booking App");
+        System.out.println("Version 2.1");
+        System.out.println("==================================");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        single.display();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
+
+        doubleRoom.display();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        suite.display();
+        System.out.println("Available: " + suiteAvailable);
+        System.out.println();
+    }
+}
+
+abstract class Room {
+
+    String type;
+    int beds;
+    int size;
+    double price;
+
+    Room(String type, int beds, int size, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq.ft");
+        System.out.println("Price per night: ₹" + price);
+    }
+}
+
+class SingleRoom extends Room {
+
+    SingleRoom() {
+        super("Single Room", 1, 200, 2500);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    DoubleRoom() {
+        super("Double Room", 2, 350, 4000);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    SuiteRoom() {
+        super("Suite Room", 3, 600, 7500);
     }
 }
